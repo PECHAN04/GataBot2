f42213fc32d1a8008c99cc372bb0ac6a673a3955
PECHAN04 <103233109+PECHAN04@users.noreply.github.com>
2022-08-07 18:15:44 -0500

    Update vaca.js

--- plugins/vaca.js
+++ plugins/vaca.js
@@ -3,16 +3,16 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-global.db.data.users[m.sender].money += 100 
-global.db.data.users[m.sender].exp += 100
+global.db.data.users[m.sender].money += 50 
+global.db.data.users[m.sender].exp += 50
   
-let vn = './media/UwU.mp3'
-conn.sendFile(m.chat, vn, 'UwU.mp3', null, m, true, {
+let vn = './media/vaca.mp3'
+conn.sendFile(m.chat, vn, 'vaca.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /UwU|uwu|Uwu|uwU|UWU/
+handler.customPrefix = /d19|D19|d019|D019|:v/
 handler.command = new RegExp
 export default handler
 
