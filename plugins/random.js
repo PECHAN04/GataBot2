53d132620a7e5fe730366a1188c3b8587224b8df
PECHAN04 <103233109+PECHAN04@users.noreply.github.com>
2022-08-08 20:10:46 -0500

    Update random.js

--- plugins/random.js
+++ plugins/random.js
@@ -4,9 +4,7 @@ import path from 'path'
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 global.db.data.users[m.sender].money += 50
-global.db.data.users[m.sender].exp += 100
-  
-
+global.db.data.users[m.sender].exp += 50
   
 let vn = './media/random.mp3'
 conn.sendFile(m.chat, vn, 'rando..mp3', null, m, true, {
@@ -14,7 +12,7 @@ type: 'audioMessage',
 ptt: true 
 })
 }
-handler.customPrefix = /UwU|uwu|Uwu|uwU|UWU/
+handler.customPrefix = /random|Random/
 handler.command = new RegExp
 export default handler
 
