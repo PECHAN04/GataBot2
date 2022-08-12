956ed5b52173ad9a8eb2afcf63b32082ee0c5f62
PECHAN04 <103233109+PECHAN04@users.noreply.github.com>
2022-08-12 16:50:02 -0500

    Update so-16sensual.js

--- plugins/so-16sensual.js
+++ plugins/so-16sensual.js
@@ -3,8 +3,8 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-global.db.data.users[m.sender].money += 100 
-global.db.data.users[m.sender].exp += 100
+global.db.data.users[m.sender].money += 50
+global.db.data.users[m.sender].exp += 50
   
 let vn = './media/sensual.mp3'
 conn.sendFile(m.chat, vn, 'sensual.mp3', null, m, true, {
@@ -12,6 +12,6 @@ type: 'audioMessage',
 ptt: true 
 })
 }
-handler.customPrefix = /d14|D14|d014|D014/ 
+handler.customPrefix = /d14|D14|7u7|7v7|d014|D014|sensual/ 
 handler.command = new RegExp
 export default handler
